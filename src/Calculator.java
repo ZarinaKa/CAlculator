ba67967bfe11619d91c599211955e88d2704b420
Zarina <zarinakargaeva@Zarinas-MacBook-Pro.local>
2019-01-19 12:39:09 -0500

    add substract method

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,4 +3,12 @@ public class Calculator {
     public static double add(double num1, double num2){
        return num1+num2;
     }
+
+    public static double substruct(double num1, double num2){
+        return num1-num2;
+
+    }
+
+
+
 }
